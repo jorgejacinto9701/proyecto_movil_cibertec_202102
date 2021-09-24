44adbb2362292ee363cc9435c273e5a7e8000faa
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2021-09-24 01:26:23 -0500

    Registro de alumno

--- app/src/main/java/com/example/proyecto_cibertec/AlumnoActivity.java
+++ app/src/main/java/com/example/proyecto_cibertec/AlumnoActivity.java
@@ -8,15 +8,71 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class AlumnoActivity extends AppCompatActivity {
 
+    EditText txtNombre,txtApellido, txtDNI, txtCelular, txtCorreo;
+    Button btnRegistra;
+
+    public static final String TEXTO = "[a-zA-ZáéíóúñüÁÉÍÓÚÑÜ\\s]{2,20}";
+    public static final String DNI = "[0-9]{8}";
+    public static final String CELULAR = "(9)[0-9]{8}";
+    public static final String CORREO = "[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})";
+    public static final String FECHA = "((19|20)\\d\\d)-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_alumno);
+
+        txtNombre = findViewById(R.id.id_alumno_nombre);
+        txtApellido = findViewById(R.id.id_alumno_apellido);
+        txtDNI = findViewById(R.id.id_alumno_dni);
+        txtCelular = findViewById(R.id.id_alumno_telefono);
+        txtCorreo = findViewById(R.id.id_alumno_correo);
+        btnRegistra = findViewById(R.id.id_alumno_registrar);
+
+        btnRegistra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                    if (!txtNombre.getText().toString().matches(TEXTO)){
+                        mensajeToast("El nombre es 2 a 20 caracteres");
+                        return;
+                    }
+                    if (!txtApellido.getText().toString().matches(TEXTO)){
+                        mensajeToast("El apellido es 2 a 20 caracteres");
+                        return;
+                    }
+                    if (!txtDNI.getText().toString().matches(DNI)){
+                        mensajeToast("El dni es de 8 dígitos");
+                        return;
+                    }
+                    if (!txtCelular.getText().toString().matches(CELULAR)){
+                        mensajeToast("El celular empieza con 9 y tiene en total 9 dígitos");
+                        return;
+                    }
+                    if (!txtCorreo.getText().toString().matches(CORREO)){
+                        mensajeToast("No tiene formato de correo");
+                        return;
+                    }
+
+                    String msg = "Los datos ingresados son \n\n";
+                    msg += "Nombre : "+ txtNombre.getText().toString() + "\n";
+                    msg += "Apellido : "+ txtApellido.getText().toString() + "\n";
+                    msg += "DNI : "+ txtDNI.getText().toString() + "\n";
+                    msg += "Celular : "+ txtCelular.getText().toString() + "\n";
+                    msg += "Correo : "+ txtCorreo.getText().toString() + "\n";
+                    mensajeAlert(msg);
+            }
+        });
+
+
+
     }
 
 
@@ -26,12 +82,12 @@ public class AlumnoActivity extends AppCompatActivity {
         return true;
     }
 
-    public void mensaje(String mensaje){
+    public void mensajeToast(String mensaje){
         Toast toast = Toast.makeText(getApplicationContext(),mensaje, Toast.LENGTH_SHORT);
         toast.show();
     }
 
-    public void mostrarMensaje(String msg){
+    public void mensajeAlert(String msg){
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         alert.setMessage(msg);
         alert.setCancelable(true);
